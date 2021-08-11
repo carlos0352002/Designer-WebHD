e484c7b4e1fba77e98e3cc6e82372841597ae5db
carlos0352002 <75769633+carlos0352002@users.noreply.github.com>
2021-08-11 01:35:26 -0400

    Add files via upload

---
+++ JS/main-java-personalizados.js
@@ -0,0 +1,12 @@
+const tont = document.querySelector('.btn-flotante');
+tont.addEventListener('click', function(){
+	document.getElementById('bk-pop').classList.toggle('posi')
+});
+
+
+const cardclose = document.querySelector('.cerracard');
+cardclose.addEventListener('click', function(){
+
+});
+
+
